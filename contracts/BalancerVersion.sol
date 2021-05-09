776455a7af3cf50d064d9d92556d68ec6348c6b9
Orisoth <31296860+Orisoth@users.noreply.github.com>
2021-05-09 04:05:00 -0400

    Balancer trade working

--- contracts/BalancerVersion.sol
+++ contracts/BalancerVersion.sol
@@ -1,2 +1,112 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.1;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import "hardhat/console.sol";
+
+interface WETH is IERC20Upgradeable {
+    function deposit() external payable;
+
+    function withdraw(uint256) external;
+}
+
+interface TokenInterface is IERC20Upgradeable {
+    function name() external returns(string memory);
+}
+
+interface Balancer {
+    struct Swap {
+        address pool;
+        address tokenIn;
+        address tokenOut;
+        uint256 swapAmount; // tokenInAmount / tokenOutAmount
+        uint256 limitReturnAmount; // minAmountOut / maxAmountIn
+        uint256 maxPrice;
+    }
+
+    function viewSplitExactIn(
+        address tokenIn,
+        address tokenOut,
+        uint256 swapAmount,
+        uint256 nPools
+    ) external view returns (Swap[] memory swaps, uint256 totalOutput);
+
+    function batchSwapExactIn(
+        Swap[] memory swaps,
+        TokenInterface tokenIn,
+        TokenInterface tokenOut,
+        uint256 totalAmountIn,
+        uint256 minTotalAmountOut
+    ) external payable returns (uint256 totalAmountOut);
+}
+
+// 0x5FC8d32690cc91D4c39d9d3abcBD16989F875707
+
+contract SwapingBalancer {
+    Balancer bal = Balancer(0x3E66B66Fd1d0b02fDa6C811Da9E0547970DB2f21);
+    WETH weth = WETH(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    address payable owner;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    function _wrapEth(uint _value) private {
+        weth.deposit{value: _value}();
+
+        if (weth.allowance(address(this), address(bal)) < _value) {
+            weth.approve(address(bal), _value);
+        }
+    }
+
+    function DOit(address _tokenAddress,uint _value, uint _slippage) internal {
+        require(_slippage < 100, "slippage cannot be more than 100%");
+        _wrapEth(_value);
+        console.log("token name %s", TokenInterface(_tokenAddress).name());
+        (Balancer.Swap[] memory swaps, uint256 totalOutput) =
+            bal.viewSplitExactIn(address(weth), _tokenAddress, _value, 2);
+            console.log(totalOutput);
+        console.log("im still standing");
+        totalOutput = bal.batchSwapExactIn(
+            swaps,
+            TokenInterface(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2),
+            TokenInterface(_tokenAddress),
+            _value,
+            totalOutput * (100 - _slippage) / 100
+        );
+        TokenInterface(_tokenAddress).transfer(msg.sender,totalOutput);
+        console.log("output recieved %s",totalOutput);
+        console.log("balance of      %s",TokenInterface(_tokenAddress).balanceOf(msg.sender));
+    }
+
+        function SwapMultiple(
+        address[] memory _addressesToSwap,
+        uint256[] memory _distribution,
+        uint256 _slipp
+    ) external payable {
+        require(_addressesToSwap.length == _distribution.length, "array size doesnt match");
+         uint _max = 100;
+         bool _continue = true;
+        for (uint256 index = 0; index < _addressesToSwap.length; index++) {
+            unchecked {
+            _distribution[index] > _max ? (_continue = false, _max = _max) : (_continue = true , _max = _max - _distribution[index]);
+            }   
+            if (!_continue) {
+                break;
+            }
+            uint _num =  (_distribution[index] * msg.value) / 100;
+            uint _fee = _num * 1 /1000;
+            _num -= _fee;
+            // console.log(_num);
+            // console.log(_fee);
+            owner.transfer(_fee);
+            DOit(
+                _addressesToSwap[index],
+               _num,
+                _slipp
+            );
+        }
+        // console.log(_max);
+        payable(msg.sender).transfer(msg.value * _max / 100);
+    }
+}
--- test/sample-test.js
+++ test/sample-test.js
@@ -1,11 +1,13 @@
 const { assert, web3 } = require("hardhat");
 const SwapingContract = artifacts.require("SwapingContract");
+const SwapingBalancer = artifacts.require("SwapingBalancer");
 const ERC20 = artifacts.require("ERC20");
 
 const UNITOKEN = "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984"
 const BNBTOKEN = "0xB8c77482e45F1F44dE1745F52C74426C631bDD52" // no direct pair
 const LINKTOKEN = "0x514910771AF9Ca656af840dff83E8264EcF986CA"
 const BTCTOKEN = "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599"
+const DAITOKEN = "0x6B175474E89094C44Da98b954EedeAC495271d0F"
 
 // Traditional Truffle test
 contract("The swaping contract bb", accounts => {
@@ -68,6 +70,77 @@ beforeEach(async () => {instance = await SwapingContract.new()})
   it("I doenst run out of gas on aprox 10", async () =>{
     try {
       const transaction =  await instance.SwapMultiple([UNITOKEN,BTCTOKEN,UNITOKEN,BTCTOKEN,BTCTOKEN,UNITOKEN,BTCTOKEN,UNITOKEN,BTCTOKEN],[10,10,10,10,10,10,10,10,10],5,{value:web3.utils.toWei("1")})
+      uni = new ERC20(UNITOKEN)
+      assert.equal(0,await web3.eth.getBalance(instance.address))
+      assert.ok(transaction)
+    } catch (error) {
+       assert.fail(error)
+     }
+  })
+
+});
+
+
+contract("The balancer swaping contract bb", accounts => {
+let instance
+
+beforeEach(async () => {instance = await SwapingBalancer.new()})
+
+  it("Should not explode ", async function() {
+    const greeter = await SwapingBalancer.new();
+    assert.ok(greeter.address);
+  });
+
+  it("should trade Ether", async () => {
+    try {
+     const transaction =  await instance.SwapMultiple([BTCTOKEN,LINKTOKEN],[50,50],5,{value:web3.utils.toWei("1")})
+     assert.isOk(transaction)
+    } catch (error) {
+      assert.fail(error)
+    }
+
+  })
+
+  it("Should not have any money left", async () =>{
+    try {
+      const transaction =  await instance.SwapMultiple([DAITOKEN,LINKTOKEN],[25,25],5,{value:web3.utils.toWei("1")})
+      // uni = new ERC20(LINKTOKEN)
+      // console.log("This is the log from the test %s",await uni.balanceOf(accounts[0]))
+      assert.equal(0,await web3.eth.getBalance(instance.address))
+     } catch (error) {
+       assert.fail(error)
+     }
+  })
+  
+  it("Swap tokens until the percentage admit then return the extra money ", async () =>{
+    try {
+      const transaction =  await instance.SwapMultiple([UNITOKEN,DAITOKEN],[80,70],5,{value:web3.utils.toWei("1")})
+      uni = new ERC20(UNITOKEN)
+      console.log("This is the log from the test %s",await uni.balanceOf(accounts[0]))
+      assert.equal(0,await web3.eth.getBalance(instance.address))
+      assert.ok(transaction)
+    } catch (error) {
+       assert.fail(error)
+     }
+  })
+
+  it("Takes fee", async () => {
+    let balance = await web3.eth.getBalance(accounts[0])
+    let transaction =  await instance.SwapMultiple([UNITOKEN,DAITOKEN,LINKTOKEN],[20,25,40],5,{value:web3.utils.toWei("1"),from:accounts[1]})
+    transaction =  await instance.SwapMultiple([UNITOKEN,DAITOKEN],[50,50],5,{value:web3.utils.toWei("1"),from:accounts[1]})
+    transaction =  await instance.SwapMultiple([UNITOKEN,DAITOKEN],[80,70],5,{value:web3.utils.toWei("1"),from:accounts[1]})
+    balance = parseInt(balance) + parseInt(web3.utils.toWei("0.85")) * 0.001 + parseInt(web3.utils.toWei("1"))* 0.001 + parseInt(web3.utils.toWei("0.8")) * 0.001
+
+    assert.equal(0,await web3.eth.getBalance(instance.address))
+    n = await web3.eth.getBalance(accounts[0])
+    console.log(web3.utils.fromWei((Math.abs(balance - parseInt(n)).toString())))
+    assert.isTrue(web3.utils.fromWei((Math.abs(balance - parseInt(n)).toString())) < 0.00000000001)
+    
+  })
+
+  it("I doenst run out of gas on aprox 10", async () =>{
+    try {
+      const transaction =  await instance.SwapMultiple([UNITOKEN,DAITOKEN,UNITOKEN,DAITOKEN,DAITOKEN,UNITOKEN,DAITOKEN,UNITOKEN,DAITOKEN],[10,10,10,10,10,10,10,10,10],5,{value:web3.utils.toWei("1")})
       uni = new ERC20(UNITOKEN)
       assert.equal(0,await web3.eth.getBalance(instance.address))
       assert.ok(transaction)
